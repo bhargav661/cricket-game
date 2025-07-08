fb82f521fd5c3d34075fb9d0fa4f181522b054ec
Bhargav Solanki <astrodevli223@gmail.com>
2025-07-08 16:09:52 +0530

    Fix: cleaned .gitignore and re-added all files

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Cricket Multiplayer Server is running!');
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
